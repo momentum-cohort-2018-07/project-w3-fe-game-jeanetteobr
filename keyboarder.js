bdb38eb5a92eb4c9ee1df391f533216bc1249055
jeanetteobr <jeanette.obr@gmail.com>
2018-08-19 15:45:53 -0400

    rewrote JS. Attempted to get player to show on canvas

--- keyboarder.js
+++ keyboarder.js
@@ -1,5 +1,63 @@
+const canvas = document.getElementById('screen')
+const context = canvas.getContext('2d')
+const screenSize = {
+  x: canvas.width,
+  y: canvas.height,
+}
+
+const colors = {
+  player: "#000000"
+}
+
+class Game {
+  constructor() {
+    this.player = new Player(this);
+  }
+  draw() {
+    context.fillRect(250, 450, 20, 20)
+    this.player.draw()
+  }
+  tick() {
+    this.update()
+    this.draw()
+    requestAnimationFrame(() => this.tick())
+  }
+
+  update() {
+    this.player.update()
+  }
+}
+
+class Player {
+  constructor(game) {
+    this.center = {
+      x: screenSize.x / 2 - 10,
+      y: 450
+    }
+    this.playerSize = {
+      x: 20,
+      y: 20
+    }
+    this.game = game
+  }
+  draw() {
+    this.game.context.fillStyle = colors.player
+    this.game.context.fillRect(this.center.x, this.center.y, this.playerSize.x, this.playerSize.y)
+  }
+  update() {
+    if (this.keyboarder.isDown(Keyboarder.KEYS.LEFT)) {
+      this.center.x -= 2
+      if (this.center.x <= 0) this.center.x = 0
+    }
+    if (this.keyboarder.isDown(Keyboarder.KEYS.RIGHT)) {
+      this.center.x += 2
+      if (this.center.x >= 480) this.center.x = 480
+    }
+  }
+}
+
 class Keyboarder {
-  constructor () {
+  constructor() {
     this.keyState = {}
 
     window.addEventListener('keydown', function (e) {
@@ -11,11 +69,11 @@ class Keyboarder {
     }.bind(this))
   }
 
-  isDown (keyCode) {
+  isDown(keyCode) {
     return this.keyState[keyCode] === true
   }
 
-  on (keyCode, callback) {
+  on(keyCode, callback) {
     window.addEventListener('keydown', function (e) {
       if (e.keyCode === keyCode) {
         callback()
@@ -24,4 +82,11 @@ class Keyboarder {
   }
 }
 
-Keyboarder.KEYS = { LEFT: 37, RIGHT: 39, UP: 38, DOWN: 40, S: 83 }
+Keyboarder.KEYS = {
+  LEFT: 37,
+  RIGHT: 39,
+  UP: 38,
+  DOWN: 40,
+  S: 83
+}
+
